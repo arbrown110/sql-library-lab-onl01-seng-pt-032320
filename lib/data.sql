8880f2e77978e9ae3d9b29dc7906665f9bf638d6
chic-caller-7520 <arbrown110@gmail.com>
2020-05-26 03:24:44 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Magicians", 1, 1), (2, "Black Lightening", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
+INSERT INTO subgenres (id, name) VALUES (1, "SYFY"), (2, "Comic");
 
-INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
+INSERT INTO authors (id, name) VALUES (1, "Sera Gamble"), (2, "Greg Berlanti");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "The Book of War", 1991, 1), (2, "The Book of Markovia", 2006, 1), (3, "The Book of Secrets", 2000, 1), (4, "Fillory and Further", 2002, 2), (5, "Purgatory", 2003, 2), (6, "Cello squirrel Daffodil", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Alice", "Quentin Please", "Niffin", 1), (2, "William", "Whatever", "Time traveler", 1), (3, "Jefferson", "I have to do it for my girls", "Conductor", 1), (4, "Khalid", "i'm stronger than this", "human", 1);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
